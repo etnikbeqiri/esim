e30da3163c2942a5a6d0fedc48c27a422bd2023f
root <root@v2202502239155317401.luckysrv.de>
2026-02-16 20:54:49 +0100

    chore: comment out paysera verification meta tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="verify-paysera" content="38d93c2b5a851fce36a1f6feb461c5c0">
+        {{-- <meta name="verify-paysera" content="38d93c2b5a851fce36a1f6feb461c5c0"> --}}
         <meta name="yandex-verification" content="db3364ee0471a212" />
         <meta name="msvalidate.01" content="E53E5C3BFF03427591C1699203F32568" />
         <meta name="cryptomus" content="7c0ee239" />
